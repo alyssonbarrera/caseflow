952c329e800a355bf6e860ba7c4846f459d280b4
Alysson Barrera <alyssonbarrera@outlook.com>
2026-02-11 17:54:23 -0300

    ♻️ improve code

--- src/modules/flow/components/delete-clue-button/delete-clue-button.tsx
+++ src/modules/flow/components/delete-clue-button/delete-clue-button.tsx
@@ -1,11 +1,8 @@
-import { memo } from "react";
 import { useDeleteClueButtonModel } from "./delete-clue-button-model";
 import { DeleteClueButtonView } from "./delete-clue-button-view";
 
 type DeleteClueButtonProps = Parameters<typeof useDeleteClueButtonModel>[0];
 
-function DeleteClueButtonComponent(props: DeleteClueButtonProps) {
+export function DeleteClueButton(props: DeleteClueButtonProps) {
   return <DeleteClueButtonView {...useDeleteClueButtonModel(props)} />;
 }
-
-export const DeleteClueButton = memo(DeleteClueButtonComponent);
